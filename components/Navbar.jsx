2f29665204d5d747b840b15560eb966df562b3e1
Akhtar Miah <akhtar.miah@nimbleapproach.com>
2023-08-04 00:07:16 +0100

    xLogo.svg ref

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,7 @@ import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from "react-icons/ai";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 // import { useRouter } from 'next/router';
-import NavLogo from "../public/assets/xlogo.svg";
+import NavLogo from "../public/assets/xLogo.svg";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
